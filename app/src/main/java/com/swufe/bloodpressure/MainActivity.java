14f19b2f93a1197c3b64cc07dae008e8300f9f5f
CMKA-ERSH <1552069369@qq.com>
2020-06-09 23:09:49 +0800

    v0.03可以通过页面上的按钮跳转了

--- app/src/main/java/com/swufe/bloodpressure/MainActivity.java
+++ app/src/main/java/com/swufe/bloodpressure/MainActivity.java
@@ -2,7 +2,9 @@ package com.swufe.bloodpressure;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void openRecord(View btn){
+        Intent recordBP = new Intent(this, recordBP.class);
+        startActivity(recordBP);
+    }
+    public void openList(View btn){
+        Intent listBP = new Intent(this, listBP.class);
+        startActivity(listBP);
     }
 }
--- app/src/main/java/com/swufe/bloodpressure/listBP.java
+++ app/src/main/java/com/swufe/bloodpressure/listBP.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class listBloodPressure extends AppCompatActivity {
+public class listBP extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_list_blood_pressure);
+        setContentView(R.layout.activity_list_b_p);
     }
 }
