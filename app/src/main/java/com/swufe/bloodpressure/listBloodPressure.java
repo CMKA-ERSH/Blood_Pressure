1028ae0832b65cbfec4a92c7d5d63df0b5589ff2
CMKA-ERSH <1552069369@qq.com>
2020-06-09 22:18:24 +0800

    v0.02初步建立好所需整体页面

---
+++ app/src/main/java/com/swufe/bloodpressure/listBloodPressure.java
@@ -0,0 +1,14 @@
+package com.swufe.bloodpressure;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class listBloodPressure extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_blood_pressure);
+    }
+}
---
+++ app/src/main/java/com/swufe/bloodpressure/recordBP.java
@@ -0,0 +1,14 @@
+package com.swufe.bloodpressure;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class recordBP extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_record_b_p);
+    }
+}
